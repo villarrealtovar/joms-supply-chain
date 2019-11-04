103a3248acdc6017b25b5c33ae54f84b07a0520c
Jose Andres Villarreal Tovar <villarrealtovar@gmail.com>
2019-11-04 13:21:20 -0500

    JOMS-002: Creating Transaction Processor

---
+++ tp/state.js
@@ -0,0 +1,35 @@
+var { _hash } = require("./lib");
+var { TP_NAMESPACE } = require("./constants");
+
+const makeAddress = (x, label) => TP_NAMESPACE + _hash(x)
+
+class JomsStoreState {
+
+    constructor(context) {
+      this.context = context;
+      this.timeout = 500;
+      this.stateEntries = {};
+    }
+   
+    setValue(value) {
+        var address = makeAddress(value);
+        var stateEntriesSend = {}
+        stateEntriesSend[address] = Buffer.from("Hello! " + value);
+        return  this.context.setState(stateEntriesSend, this.timeout).then(function(result) {
+            console.log("Success", result)
+        }).catch(function(error) {
+            console.error("Error", error)
+        })
+    }
+
+    getValue(value) {
+        var address = makeAddress(value);
+        return  this.context.getState([address], this.timeout).then(function(stateEntries) {
+          Object.assign(this.stateEntries, stateEntries);
+          console.log(this.stateEntries[address].toString())
+          return  this.stateEntries;
+        }.bind(this))
+      }
+}
+
+module.exports = JomsStoreState;
